96c20bb778ac45db652e763549cd3a3ae679c488
Sio Jurnalis Pipin <siojurnalispipin@hotmail.com>
2022-07-14 15:39:18 +0700

    up

--- api/routes/service.js
+++ api/routes/service.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const serviceCtrl = require("../controllers/service");
 const checkAuth = require("../middleware/check-auth");
 
-router.get("/", checkAuth, serviceCtrl.services);
+router.get("/", serviceCtrl.services);
 router.get("/:serviceId", checkAuth, serviceCtrl.serviceDetail);
 router.post("/", checkAuth, serviceCtrl.servicesAdd);
 router.put("/:serviceId", checkAuth, serviceCtrl.servicesEdit);
