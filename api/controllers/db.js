9f59fae7aed1e2ed38d1625b345c09eaf3b37a9f
Sio Jurnalis Pipin <siojurnalispipin@hotmail.com>
2022-07-31 17:50:10 +0700

    db

--- api/controllers/db.js
+++ api/controllers/db.js
@@ -1,11 +1,22 @@
 const mysql = require("mysql2/promise");
 const config = require("../../config");
 
+let globalPool = undefined;
+
 async function query(sql, params) {
-  const connection = await mysql.createPool(config.db);
-  const [result] = await connection.execute(sql, params);
+  const conn = await connect();
+  const [result] = await conn.execute(sql, params);
 
   return result;
+}
+
+async function connect() {
+  if (globalPool) {
+    return globalPool;
+  }
+
+  globalPool = await mysql.createPool(config.db);
+  return globalPool;
 }
 
 module.exports = { query };
